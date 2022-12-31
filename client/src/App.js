c56b31adb35d4fbd0cfeb0287e2699fcffc61ab2
Batel Haiylo <inpo686@gmail.com>
2022-12-31 20:28:51 +0200

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
-import Router from "./components/router/Router";
-import NavBar from "./components/navbar/NavBar";
-import Footer from "./components/footer/Footer";
 
-function App() {
+
+export default function App() {
   return (
     <div className="app">
-      <NavBar/>
-      <Router/>
-      <Footer/>
     </div>
   );
-}
-
-export default App;
+};
